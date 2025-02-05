534ff99f9142d5208e0fb87a44e0eb40a9d4e643
HETANG404 <hetang401@gmail.com>
2025-02-05 15:46:44 +0800

    fix lombok

--- mobile-backend/src/main/java/com/tang/demo_db/entity/Feedback.java
+++ mobile-backend/src/main/java/com/tang/demo_db/entity/Feedback.java
@@ -1,11 +1,17 @@
 package com.tang.demo_db.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "feedback")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Feedback {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- mobile-backend/src/main/java/com/tang/demo_db/entity/QueryLog.java
+++ mobile-backend/src/main/java/com/tang/demo_db/entity/QueryLog.java
@@ -2,11 +2,17 @@ package com.tang.demo_db.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "query_log")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class QueryLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
